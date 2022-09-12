ec7a0bc43c6429aad0c8ddb10a3991879b7577d9
Rachit Pal <rachit2380@gmail.com>
2022-09-12 19:39:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+const init = (n) => {
+  slides.forEach((slide, index) => {
+    slide.style.display = "none";
+    dots.forEach((dot, index) => {
+      dot.classList.remove("active");
+    });
+  });
+  slides[n].style.display = "block";
+  dots[n].classList.add("active");
+};
+document.addEventListener("DOMContentLoaded", init(currentSlide));
+const next = () => {
+  currentSlide >= slides.length - 1 ? (currentSlide = 0) : currentSlide++;
+  init(currentSlide);
+};
+
+const prev = () => {
+  currentSlide <= 0 ? (currentSlide = slides.length - 1) : currentSlide--;
+  init(currentSlide);
+};
+
+document.querySelector(".next").addEventListener("click", next);
+
+document.querySelector(".prev").addEventListener("click", prev);
+
+setInterval(() => {
+  next();
+}, 5000);
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    console.log(currentSlide);
+    init(i);
+    currentSlide = i;
+  });
+});
